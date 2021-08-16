ccae883095c6fe04c4d136191fca0e7e8d45db7e
Daniel Burger <public@danielburger.online>
2021-08-16 14:10:47 +0200

    Changing server configs

--- problem_2/src/server.ts
+++ problem_2/src/server.ts
@@ -1,18 +1,18 @@
-import config from 'config'
-import connectDB from './config/db/databse'
+// import config from 'config'
+import connectDB from './config/db/database'
 import app from './app'
 
-const serverConfig: any = config.get('server')
-const dbConfig: any = config.get('db')
+// const serverConfig: any = config.get('server')
+// const dbConfig: any = config.get('db')
 
 connectDB()
   .then(() => {
     console.log(`Connected To Database`)
   })
-  .catch(err => {
+  .catch((err: string) => {
     console.log('Error Connecting To Database', err)
   })
 
-app.listen(serverConfig.port, () => {
-  console.info(`Listening on port ${serverConfig.port}`)
+app.listen(process.env.PORT, () => {
+  console.info(`Listening on port ${process.env.PORT}`)
 })
