421c62ee460af270a72cb4a00f6f479b0c6390d3
Daniel Burger <public@danielburger.online>
2021-08-17 11:59:04 +0200

    Adding TODOs and icons

--- problem_1/src/views/experiment/components/Completed.tsx
+++ problem_1/src/views/experiment/components/Completed.tsx
@@ -1,8 +1,20 @@
+import { useRouter } from 'next/router'
+
 export default function Countdown(): JSX.Element {
+  const router = useRouter()
   return (
     <>
       <main>
         <h1>Experiment complete. Good job!</h1>
+        <button onClick={() => router.push('/')}>Go back home</button>
+        <button
+          onClick={() =>
+            // TODO Create the functionality to download the experiment data
+            alert("Functionality for downloading data hasn't been added yet")
+          }
+        >
+          Download data
+        </button>
       </main>
     </>
   )
--- problem_1/src/views/experiment/components/Config.tsx
+++ problem_1/src/views/experiment/components/Config.tsx
@@ -2,6 +2,7 @@ export default function Config(): JSX.Element {
   return (
     <>
       <main>
+        {/* TODO Create a config component */}
         <h1>Config</h1>
       </main>
     </>
